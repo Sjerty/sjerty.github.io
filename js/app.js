dcde81303951a6bc770bb08893c2a5592bb21dcb
Sjerty <Sj3rt4@gmail.com>
2020-05-12 23:00:28 +0400

    Добавил файлы

---
+++ js/app.js
@@ -0,0 +1,68 @@
+$(function(){
+
+    let filter = $("[data-filter]");
+    
+    filter.on("click", function(event) {
+        event.preventDefault();
+         
+        let cat = $(this).data('filter');
+        if(cat == 'all'){
+            $("[data-cat]").removeClass("hide");
+        } else {
+
+            $("[data-cat]").each(function(){
+                
+                let workCat = $(this).data('cat');
+
+                if(workCat != cat){
+                    $(this).addClass('hide');
+                } else{
+                    $(this).removeClass('hide');
+                }
+
+            });
+        }
+    });
+    
+   /*Modal*/ 
+    
+    const modal_call = $("[data-modal]");
+    const modal_close = $("[data-close]");
+    
+    modal_call.on("click", function(event){
+        event.preventDefault();
+        
+        let $this = $(this);
+        let modalId = $this.data('modal');
+        
+        $(modalId).addClass('show');
+        $('body').addClass('no-scroll');
+        
+        setTimeout(function(){
+            $(modalId).find(".modal__dialog").css({
+                transform: "rotateX(0)"
+            })
+        },200);
+    });
+    
+    modal_close.on("click", function(event){
+        event.preventDefault();
+        
+        let $this = $(this);
+        let modalPar = $this.parents('.modal');
+        
+        modalPar.removeClass('show');
+        $('body').removeClass('no-scroll');
+    });
+    
+    $(".modal").on("click", function(event){
+        $(this).removeClass('show');
+        $('body').removeClass('no-scroll');
+    });
+    
+    $(".modal__dialog").on("click", function(event){
+        event.stopPropagation();
+    });
+    
+
+});
